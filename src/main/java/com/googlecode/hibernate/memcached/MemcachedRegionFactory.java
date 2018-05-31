e7b0fbf9f5e6a4873f457fd0e032eb1e27eab561
zhangyinhao <zhangyinhao1234@163.com>
2018-05-31 16:32:10 +0800

    修改支持hibernate 5.3.1.Final的二级缓存

--- src/main/java/com/googlecode/hibernate/memcached/MemcachedRegionFactory.java
+++ src/main/java/com/googlecode/hibernate/memcached/MemcachedRegionFactory.java
@@ -15,115 +15,145 @@
 
 package com.googlecode.hibernate.memcached;
 
-import com.googlecode.hibernate.memcached.region.*;
 import org.hibernate.boot.spi.SessionFactoryOptions;
 import org.hibernate.cache.CacheException;
+import org.hibernate.cache.cfg.spi.DomainDataRegionBuildingContext;
+import org.hibernate.cache.cfg.spi.DomainDataRegionConfig;
+import org.hibernate.cache.cfg.spi.EntityDataCachingConfig;
 import org.hibernate.cache.spi.*;
 import org.hibernate.cache.spi.access.AccessType;
+import org.hibernate.cache.spi.support.DomainDataStorageAccess;
+import org.hibernate.cache.spi.support.RegionFactoryTemplate;
+import org.hibernate.cache.spi.support.RegionNameQualifier;
+import org.hibernate.cache.spi.support.StorageAccess;
+import org.hibernate.engine.spi.SessionFactoryImplementor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Constructor;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
+public class MemcachedRegionFactory extends RegionFactoryTemplate {
 
-public class MemcachedRegionFactory implements RegionFactory {
+	private final Logger log = LoggerFactory.getLogger(MemcachedRegionFactory.class);
 
-    private final Logger log = LoggerFactory.getLogger(MemcachedRegionFactory.class);
+	private final ConcurrentMap<String, MemcachedCache> caches = new ConcurrentHashMap<String, MemcachedCache>();
 
-    private final ConcurrentMap<String, MemcachedCache> caches = new ConcurrentHashMap<String, MemcachedCache>();
+	private Properties properties;
+	private Memcache client;
 
-    private Properties properties;
-    private Memcache client;
-    private SessionFactoryOptions settings;
+	public MemcachedRegionFactory(Properties properties) {
+		this.properties = properties;
+	}
 
-    public MemcachedRegionFactory(Properties properties) {
-        this.properties = properties;
-    }
+	public MemcachedRegionFactory() {
+	}
 
-    public MemcachedRegionFactory() {
-    }
+	/**
+	 * start
+	 */
+	@Override
+	protected void prepareForUse(SessionFactoryOptions settings, Map configValues) {
+		this.properties = toProperties(configValues);
+		log.info("Starting MemcachedClient...");
+		try {
+			client = getMemcachedClientFactory(new Config(new PropertiesHelper(properties))).createMemcacheClient();
+		} catch (Exception e) {
+			throw new CacheException("Unable to initialize MemcachedClient", e);
+		}
 
-    @Override
-    public void start(SessionFactoryOptions settings, Properties properties) throws CacheException {
+	}
 
-        this.settings = settings;
-        this.properties = properties;
-        log.info("Starting MemcachedClient...");
-        try {
-            client = getMemcachedClientFactory(new Config(new PropertiesHelper(properties))).createMemcacheClient();
-        } catch (Exception e) {
-            throw new CacheException("Unable to initialize MemcachedClient", e);
-        }
-    }
+	private Properties toProperties(Map configValues) {
+		final Properties properties = new Properties();
+		properties.putAll(configValues);
+		return properties;
+	}
 
+	/**
+	 * stop
+	 */
+	@Override
+	protected void releaseFromUse() {
+		if (client != null) {
+			log.debug("Shutting down Memcache client");
+			client.shutdown();
+		}
+		client = null;
+	}
 
+	public boolean isMinimalPutsEnabledByDefault() {
+		return true;
+	}
 
-    public void stop() {
-        if (client != null) {
-            log.debug("Shutting down Memcache client");
-            client.shutdown();
-        }
-        client = null;
-    }
+	public AccessType getDefaultAccessType() {
+		return AccessType.READ_WRITE;
+	}
 
-    public boolean isMinimalPutsEnabledByDefault() {
-        return true;
-    }
+	public long nextTimestamp() {
+		return System.currentTimeMillis() / 100;
+	}
 
-    public AccessType getDefaultAccessType() {
-        return AccessType.READ_WRITE;
-    }
+	protected MemcacheClientFactory getMemcachedClientFactory(Config config) {
+		String factoryClassName = config.getMemcachedClientFactoryName();
 
-    public long nextTimestamp() {
-        return System.currentTimeMillis() / 100;
-    }
+		Constructor<?> constructor;
+		try {
+			constructor = Class.forName(factoryClassName).getConstructor(PropertiesHelper.class);
+		} catch (ClassNotFoundException e) {
+			throw new CacheException("Unable to find factory class [" + factoryClassName + "]", e);
+		} catch (NoSuchMethodException e) {
+			throw new CacheException(
+					"Unable to find PropertiesHelper constructor for factory class [" + factoryClassName + "]", e);
+		}
 
-    public EntityRegion buildEntityRegion(String regionName, Properties properties, CacheDataDescription metadata) throws CacheException {
-        return new MemcachedEntityRegion(getCache(regionName), settings, metadata, properties, client);
-    }
+		MemcacheClientFactory clientFactory;
+		try {
+			clientFactory = (MemcacheClientFactory) constructor.newInstance(config.getPropertiesHelper());
+		} catch (Exception e) {
+			throw new CacheException("Unable to instantiate factory class [" + factoryClassName + "]", e);
+		}
 
-    public NaturalIdRegion buildNaturalIdRegion(String regionName, Properties properties, CacheDataDescription metadata) throws CacheException {
-        return new MemcachedNaturalIdRegion(getCache(regionName), settings, metadata, properties, client);
-    }
+		return clientFactory;
+	}
 
-    public CollectionRegion buildCollectionRegion(String regionName, Properties properties, CacheDataDescription metadata) throws CacheException {
-        return new MemcachedCollectionRegion(getCache(regionName), settings, metadata, properties, client);
-    }
+	private MemcachedCache getCache(String regionName) {
+		return caches.get(regionName) == null
+				? new MemcachedCache(regionName, client, new Config(new PropertiesHelper(properties)))
+				: caches.get(regionName);
+	}
 
-    public QueryResultsRegion buildQueryResultsRegion(String regionName, Properties properties) throws CacheException {
-        return new MemcachedQueryResultsRegion(getCache(regionName));
-    }
+	protected DomainDataStorageAccess createDomainDataStorageAccess(DomainDataRegionConfig regionConfig,
+			DomainDataRegionBuildingContext buildingContext) {
+		return new StorageAccessImpl(
+				getOrCreateCache(regionConfig.getRegionName(), buildingContext.getSessionFactory()));
+	}
 
-    public TimestampsRegion buildTimestampsRegion(String regionName, Properties properties) throws CacheException {
-        return new MemcachedTimestampsRegion(getCache(regionName));
-    }
+	@Override
+	protected StorageAccess createQueryResultsRegionStorageAccess(String regionName,
+			SessionFactoryImplementor sessionFactory) {
+		return new StorageAccessImpl(getCache(regionName));
+	}
 
-    protected MemcacheClientFactory getMemcachedClientFactory(Config config) {
-        String factoryClassName = config.getMemcachedClientFactoryName();
+	@Override
+	protected StorageAccess createTimestampsRegionStorageAccess(String regionName,
+			SessionFactoryImplementor sessionFactory) {
+		return new StorageAccessImpl(getCache(regionName));
+	}
 
-        Constructor<?> constructor;
-        try {
-            constructor = Class.forName(factoryClassName).getConstructor(PropertiesHelper.class);
-        } catch (ClassNotFoundException e) {
-            throw new CacheException("Unable to find factory class [" + factoryClassName + "]", e);
-        } catch (NoSuchMethodException e) {
-            throw new CacheException("Unable to find PropertiesHelper constructor for factory class [" + factoryClassName + "]", e);
-        }
+	protected MemcachedCache getOrCreateCache(String unqualifiedRegionName, SessionFactoryImplementor sessionFactory) {
+		verifyStarted();
+		final MemcachedCache cache = caches.get(unqualifiedRegionName);
+		if (cache == null) {
+			return getCache(unqualifiedRegionName);
+		}
+		return cache;
+	}
 
-        MemcacheClientFactory clientFactory;
-        try {
-            clientFactory = (MemcacheClientFactory) constructor.newInstance(config.getPropertiesHelper());
-        } catch (Exception e) {
-            throw new CacheException("Unable to instantiate factory class [" + factoryClassName + "]", e);
-        }
-
-        return clientFactory;
-    }
-
-    private MemcachedCache getCache(String regionName) {
-        return caches.get(regionName) == null ? new MemcachedCache(regionName, client, new Config(new PropertiesHelper(properties))) : caches.get(regionName);
-    }
 }
